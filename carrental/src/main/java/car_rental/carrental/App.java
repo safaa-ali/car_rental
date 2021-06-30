d2963d392c55f44cc59b310ba9ff218772546e62
safaa ali <safaaaly246@gmail.com>
2021-06-30 13:39:56 +0200

    adding maven buliding

---
+++ carrental/src/main/java/car_rental/carrental/App.java
@@ -0,0 +1,13 @@
+package car_rental.carrental;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
